9db01449822f8b609decffbd94b117782b6af973
odan <odan3240@gmail.com>
2021-09-12 16:06:18 +0900

    add cypress

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,10 +7,9 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:jest/all",
     "prettier",
   ],
-  plugins: ["jest", "@typescript-eslint"],
+  plugins: ["@typescript-eslint"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     sourceType: "module",
